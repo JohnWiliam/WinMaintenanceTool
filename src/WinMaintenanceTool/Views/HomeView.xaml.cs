f9f825b6ad38a772d4099c979c034a41704be168
John Wiliam <djhonathan.wilian@gmail.com>
2026-03-29 20:17:43 -0400

    Create HomeView.xaml.cs

---
+++ src/WinMaintenanceTool/Views/HomeView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WinMaintenanceTool.Views;
+
+public partial class HomeView : UserControl
+{
+    public HomeView()
+    {
+        InitializeComponent();
+    }
+}
