23a0b83aa96c5c8e18ac0c40d50b1060dc315470
John Wiliam <djhonathan.wilian@gmail.com>
2026-04-02 20:28:18 -0400

    Update ViewModelBase.cs

--- src/WinMaintenanceTool/ViewModels/ViewModelBase.cs
+++ src/WinMaintenanceTool/ViewModels/ViewModelBase.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
@@ -7,17 +8,25 @@ public abstract class ViewModelBase : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
 
-    protected void SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+    /// <summary>
+    /// Atualiza o valor da propriedade e dispara o evento PropertyChanged caso o valor tenha mudado.
+    /// </summary>
+    /// <returns>Retorna true se o valor foi alterado; caso contrário, false.</returns>
+    protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
     {
         if (EqualityComparer<T>.Default.Equals(storage, value))
         {
-            return;
+            return false;
         }
 
         storage = value;
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        RaisePropertyChanged(propertyName);
+        return true;
     }
 
+    /// <summary>
+    /// Dispara o evento PropertyChanged manualmente.
+    /// </summary>
     protected void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
         => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
